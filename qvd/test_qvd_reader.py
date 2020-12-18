eb2430c8f2642a108acb4464ad9f0e1ed6e5f8c3
Sam Bentley <samtbentley@gmail.com>
2020-12-18 17:26:59 +0000

    Added shape test

--- qvd/test_qvd_reader.py
+++ qvd/test_qvd_reader.py
@@ -1,10 +1,16 @@
 from qvd import qvd_reader
 import pandas as pd
-import pytest
+import numpy
 
 
 class TestQvdReader():
     def test_read_shape(self):
         qvd = qvd_reader.read('AAPL.qvd')
         csv = pd.read_csv('AAPL.csv', float_precision='round_trip')
+        print(qvd.columns)
         assert qvd.shape == csv.shape
+
+    def test_read_size(self):
+        qvd = qvd_reader.read('AAPL.qvd')
+        csv = pd.read_csv('AAPL.csv', float_precision='round_trip')
+        assert numpy.array_equal(qvd.columns, csv.columns)
