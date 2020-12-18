b470ac77e626e85982567fc0460d0dd070bcf102
Sam Bentley <samtbentley@gmail.com>
2020-12-18 17:32:52 +0000

    remove print from test

--- qvd/test_qvd_reader.py
+++ qvd/test_qvd_reader.py
@@ -7,7 +7,6 @@ class TestQvdReader():
     def test_read_shape(self):
         qvd = qvd_reader.read('AAPL.qvd')
         csv = pd.read_csv('AAPL.csv', float_precision='round_trip')
-        print(qvd.columns)
         assert qvd.shape == csv.shape
 
     def test_read_size(self):
