4093ae78036a52c6a009e083ba84d8938b5743d2
Sam Bentley <samtbentley@gmail.com>
2022-09-16 22:20:22 +0100

    make test python 3.5 friendly

--- qvd/test_qvd_reader.py
+++ qvd/test_qvd_reader.py
@@ -1,17 +1,16 @@
 from qvd import qvd_reader
+import os
 import pandas as pd
-import numpy
+import numpy as np
 
 
 class TestQvdReader():
     def test_read_shape(self):
-        qvd = qvd_reader.read('test_files/AAPL.qvd')
+        qvd = qvd_reader.read(f'{os.path.dirname(__file__)}/test_files/AAPL.qvd')
         csv = pd.read_csv('test_files/AAPL.csv', float_precision='round_trip')
         assert qvd.shape == csv.shape
 
     def test_read_size(self):
-        qvd = qvd_reader.read('test_files/AAPL.qvd')
+        qvd = qvd_reader.read(f'{os.path.dirname(__file__)}/test_files/AAPL.qvd')
         csv = pd.read_csv('test_files/AAPL.csv', float_precision='round_trip')
-        print(qvd.columns)
-        print(csv.columns)
-        assert numpy.array_equal(qvd.columns, csv.columns)
+        assert np.array_equal(np.sort(qvd.columns, axis=0), np.sort(csv.columns, axis=0))
